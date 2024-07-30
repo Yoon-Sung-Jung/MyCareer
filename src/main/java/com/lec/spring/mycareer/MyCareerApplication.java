5cb0c2075f22e880bac754990ca6f5f80d383606
jung-yoon-sung <mrj951230@naver.com>
2024-07-30 09:48:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/lec/spring/mycareer/MyCareerApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring.mycareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCareerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCareerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lec/spring/mycareer/MyCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.spring.mycareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCareerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
