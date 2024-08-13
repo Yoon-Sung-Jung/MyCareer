4b47d896613ad2321141afc9b82f1e5e5e13eb98
jung-yoon-sung <mrj951230@naver.com>
2024-08-13 10:47:14 +0900

    Test 후, 결과 페이지로 이동(오류)

--- MyCareerClient/my-app/src/Pages/Test.js
+++ MyCareerClient/my-app/src/Pages/Test.js
@@ -15,6 +15,8 @@ const Test = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    const [answers, setAnswers] = useState({});
+
     const [curPage, setCurPage] = useState(1);
     const itemsPerPage = 10;
 
@@ -57,41 +59,58 @@ const Test = () => {
     const curItems = data.slice(indexofFirstItem, indexOfLastItem);
     const totalPages = Math.ceil(data.length / itemsPerPage);
 
-    const answers = "";
 
 
     const changeValue = (e) => {
-        
-        // q가 30, 31일 경우, answer 값은 A1=3 A2=2 ...
-        if(q == 30 || q == 31) {
-            answers += `A{e.target.name}={e.target.value} `
-        }
+        const {name, value} = e.target;
+        setAnswers((prevAnswers) => ({
+            ...prevAnswers,
+            [name]: value
+        }));
+    }
 
-        // q가 8, 9, 10 일 경우, answer 값은 2,2,2,3,...
-        if (q == 8 || q == 9 || q == 10) {
 
-        }
 
-        // q가 19, 20, 21, 22, 23, 24, 25, 26, 27 일 경우, answer 값은 1=5 2=4 3=4 ...
-        if (q == 19 || q == 20 || q == 21 || q == 22 || 
-            q == 23 || q == 24 || q == 25 || q == 26 || 
-            q == 27) {
+
+    const prevPage = () => {
+        setCurPage(prev => Math.max(prev - 1, 1));
+    }
+
+    const nextPage = () => {
+        setCurPage(prev => Math.min(prev + 1, totalPages))
+    }
+
+
+    const testResult = (e) => {
+        e.preventDefault();
+        console.log(answers);
+
+        let formattedAnswers = "";
+        for (const [key, value] of Object(answers)) {
+
+            // q가 30, 31일 경우, answer 값은 A1=3 A2=2 ...
+            if (q == 30 || q == 31) {
 
             }
 
-        // q가 6 일 경우, answer 값은 B1=1 B2=4
-        if( q == 6) {
+            // q가 8, 9, 10 일 경우, answer 값은 2,2,2,3,...
+            if (q == 8 || q == 9 || q == 10) {
 
+            }
+
+            // q가 19, 20, 21, 22, 23, 24, 25, 26, 27 일 경우, answer 값은 1=5 2=4 3=4 ...
+            if (q == 19 || q == 20 || q == 21 || q == 22 ||
+                q == 23 || q == 24 || q == 25 || q == 26 ||
+                q == 27) {
+
+            }
+
+            // q가 6 일 경우, answer 값은 B1=1 B2=4
+            if (q == 6) {
+
+            }
+            answers = answers.trimEnd();
         }
-        answers = answers.trimEnd();
-        
-    }
-
-    const testResult = (e) => {
-        e.preventDefault(); 
-        console.log(answers);
-        
-
 
         axios({
             method: "post",
@@ -106,102 +125,122 @@ const Test = () => {
                 "grade": "2",
                 "email": "",
                 "startDtm": 1550466291034,
-                "answers": "1=5 2=7 3=3 4=7 5=1 6=2 7=1 8=5 9=5 10=1 11=4 12=4 13=5 14=4 15=4 16=4 17=4 18=5 19=1 20=1 21=1 22=5 23=3 24=6 25=3 26=2 27=2 28=6 29=3 30=2 31=4 32=3 33=5 34=2 35=3 36=2 37=7 38=2 39=5 40=5 41=5 42=1 43=7 44=6 45=5 46=4 47=2 48=5 49=4 50=5 51=5 52=5 53=7 54=2 55=6 56=4 57=6 58=4 59=3 60=5 61=5 62=5 63=7 64=4 65=7 66=5"
+                "answers": answers
             }
         })
-        .then((response) => {
-            const url = response.data.RESULT.url;
-            window.location.href = url;
-        })
+            .then((response) => {
+                const url = response.data.RESULT.url;
+                window.location.href = url;
+            })
 
+
+
+        return (
+            <>
+            <h4
+                <Form onSubmit={testResult}>
+
+                    {curItems.map((item) => (
+                        <div key={item.qitemNo}>
+                            <h4>{item.qitemNo}.</h4>
+                            <h4>{item.question}</h4>
+                            <ul>
+                                {item.answer01 !== "null" && (
+                                    <li>
+                                        <label>
+                                            <input type="radio"
+                                                name={`${item.qitemNo}`}
+                                                value={item.answer01}
+                                                />
+                                            {item.answer01}
+                                        </label>
+                                    </li>
+                                )}
+                                {item.answer02 !== "null" && (
+                                    <li>
+                                        <label>
+                                            <input type="radio"
+                                                name={`${item.qitemNo}`}
+                                                value={item.answer02}
+                                                 />
+                                            {item.answer02}
+                                        </label>
+                                    </li>
+                                )}
+                                {item.answer03 !== "null" && (
+                                    <li>
+                                        <label>
+                                            <input type="radio"
+                                                name={`${item.qitemNo}`}
+                                                value={item.answer03}
+                                               />
+                                            {item.answer03}
+                                        </label>
+                                    </li>
+                                )}
+                                {item.answer04 !== "null" && (
+                                    <li>
+                                        <label>
+                                            <input type="radio"
+                                                name={`${item.qitemNo}`}
+                                                value={item.answer04}
+                                                />
+                                            {item.answer04}
+                                        </label>
+                                    </li>
+                                )}
+                                {item.answer05 !== "null" && (
+                                    <li>
+                                        <label>
+                                            <input type="radio"
+                                                name={`${item.qitemNo}`}
+                                                value={item.answer05}
+                                                 />
+                                            {item.answer05}
+                                        </label>
+                                    </li>
+                                )}
+                                {item.answer06 !== "null" && (
+                                    <li>
+                                        <label>
+                                            <input type="radio"
+                                                name={`${item.qitemNo}`}
+                                                value={item.answer06}
+                                                />
+                                            {item.answer06}
+                                        </label>
+                                    </li>
+                                )}
+                                {item.answer07 !== "null" && (
+                                    <li>
+                                        <label>
+                                            <input type="radio"
+                                                name={`${item.qitemNo}`}
+                                                value={item.answer07} />
+                                            {item.answer07}
+                                        </label>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    ))}
+
+                    {/* Pagination 설정 */}
+                    <div>
+                        {curPage === 1 ||
+                            (<Button onClick={prevPage} disabled={curPage === 1}>이전</Button>)
+                        }
+                        {curPage} / {totalPages}
+                        {curPage === totalPages ||
+                            <Button onClick={nextPage} disabled={curPage === totalPages}>다음</Button>
+                        }
+                    </div>
+                    {curPage === totalPages &&
+                        <Button type="submit">제출</Button>
+                    }
+                </Form>
+            </>
+        );
+    };
 }
-
-return (
-    <>
-        <Form onSubmit={testResult}>
-
-            {curItems.map((item) => (
-                <div key={item.qitemNo}>
-                    <h4>{item.qitemNo}.</h4>
-                    <h4>{item.question}</h4>
-                    <ul>
-                        {item.answer01 !== "null" && (
-                            <li>
-                                <label>
-                                    <input type="radio" name={`${item.qitemNo}`} value={item.answer01} />
-                                    {item.answer01}
-                                </label>
-                            </li>
-                        )}
-                        {item.answer02 !== "null" && (
-                            <li>
-                                <label>
-                                    <input type="radio" name={`${item.qitemNo}`} value={item.answer02} />
-                                    {item.answer02}
-                                </label>
-                            </li>
-                        )}
-                        {item.answer03 !== "null" && (
-                            <li>
-                                <label>
-                                    <input type="radio" name={`${item.qitemNo}`} value={item.answer03} />
-                                    {item.answer03}
-                                </label>
-                            </li>
-                        )}
-                        {item.answer04 !== "null" && (
-                            <li>
-                                <label>
-                                    <input type="radio" name={`${item.qitemNo}`} value={item.answer04} />
-                                    {item.answer04}
-                                </label>
-                            </li>
-                        )}
-                        {item.answer05 !== "null" && (
-                            <li>
-                                <label>
-                                    <input type="radio" name={`${item.qitemNo}`} value={item.answer05} />
-                                    {item.answer05}
-                                </label>
-                            </li>
-                        )}
-                        {item.answer06 !== "null" && (
-                            <li>
-                                <label>
-                                    <input type="radio" name={`${item.qitemNo}`} value={item.answer06} />
-                                    {item.answer06}
-                                </label>
-                            </li>
-                        )}
-                        {item.answer07 !== "null" && (
-                            <li>
-                                <label>
-                                    <input type="radio" name={`${item.qitemNo}`} value={item.answer07} />
-                                    {item.answer07}
-                                </label>
-                            </li>
-                        )}
-                    </ul>
-                </div>
-            ))}
-
-            {/* Pagination 설정 */}
-            <div>
-                {curPage === 1 ||
-                    (<Button onClick={() => setCurPage(prev => Math.max(prev - 1, 1))}>이전</Button>)
-                }
-                {curPage} / {totalPages}
-                {curPage === totalPages ||
-                    <Button onClick={() => setCurPage(prev => Math.min(prev + 1, totalPages))} disabled={curPage === totalPages}>다음</Button>
-
-                }
-            </div>
-            {curPage === totalPages &&
-                <Button type="submit">제출</Button>
-            }
-        </Form>
-    </>
-);
-};
-
 export default Test;
