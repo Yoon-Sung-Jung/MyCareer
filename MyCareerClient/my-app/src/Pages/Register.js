3548f5a8eee651d5b45b893998d63afb52904c75
jung-yoon-sung <mrj951230@naver.com>
2024-08-23 12:08:35 +0900

    회원가입 구현. 에러 처리 필요

--- MyCareerClient/my-app/src/Pages/Register.js
+++ MyCareerClient/my-app/src/Pages/Register.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
-
-const registerUser = (e) => {
-    e.preventDefault();
-
-}
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        username: '',
+        name: '',
+        password: '',
+        emailID: '',
+        emailDomain: ''
+    });
+    
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setUser ({
+            ...user,
+            [name]: value
+        })
+    }
+    
+    
+    const registerUser = (e) => {
+        e.preventDefault();
+        
+        axios({
+            method: "post",
+            url: "http://localhost:8080/register",
+            data: {
+                "username": user.username,
+                "name": user.name,
+                "password": user.password,
+                "email": `${user.emailID}@${user.emailDomain}`,
+                "identity": '일반'
+            }
+        })
+        .then((response) => {
+            const {data, status} = response;
+            if(status === 200) {
+                alert('가입 완료');
+                navigate(`/login`)
+            } else {
+                alert('가입에 실패하였습니다.')
+                // todo
+            }
+    
+        })
+    
+    }
     return (
         <>
             <Form onSubmit={registerUser}>
                 <label htmlFor='username'>아이디</label>
-                <input type="text" name="username" id="username"/>
-                <Button>중복 확인</Button>
+                <input type="text" name="username" id="username" value={user.username} onChange={handleChange}/>
+                <Button onClick="">중복 확인</Button>
                 <label htmlFor='name'>이름</label>
-                <input type="text" name="name" id="name"/>
+                <input type="text" name="name" id="name" value={user.name} onChange={handleChange}/>
+                <label>비밀번호</label>
+                <input type="password" name="password" id="password" value={user.password} onChange={handleChange}/>
                 <label>이메일</label>
-                <input type="text" name="emailID" id="emailID"/>
+                <input type="text" name="emailID" id="emailID" value={user.emailID} onChange={handleChange}/>
                 @
-                <input type="text" name="emailDomain" id="emailDomain"/>
+                <input type="text" name="emailDomain" id="emailDomain" value={user.emailDomain} onChange={handleChange}/>
 
                 <Button type="submit">회원가입</Button>
 
